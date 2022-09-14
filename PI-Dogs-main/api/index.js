c27a25773b6f218b1074fcbea1148fbb57a03912
Francopress26 <65971988+Francopress26@users.noreply.github.com>
2022-09-14 20:32:19 -0300

    deploy

--- PI-Dogs-main/api/index.js
+++ PI-Dogs-main/api/index.js
@@ -19,7 +19,7 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
-const PORT= process.env.PORT || 1234
+const PORT= process.env.PORT || 3001
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
   server.listen(PORT, () => {
