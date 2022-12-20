addf217ac0e89c9dd150e05225eb06b0900a2e33
Francopress26 <francopress26@hotmail.com>
2022-12-20 13:06:01 -0300

    Try deploy

--- PI-Dogs-main/api/src/db.js
+++ PI-Dogs-main/api/src/db.js
@@ -13,23 +13,7 @@ const {
 const basename = path.basename(__filename);
 
 // para poder deployear
-const sequelize =
-  process.env.NODE_ENV === "production"
-    ? new Sequelize({
-        database: DB_NAME,
-        dialect: "postgres",
-        host: DB_HOST,
-        port: 5432,
-        username: DB_USER,
-        password: DB_PASSWORD,
-        pool: { max: 3, min: 1, idle: 10000 },
-        dialectOptions: {
-          ssl: { require: true, rejectUnauthorized: false },
-          keepAlive: true,
-        },
-        ssl: true,
-      })
-    : new Sequelize(
+const sequelize = new Sequelize(
       "postgres://yeibjham:bwura8uzawWTEV58OM3Jq1KmYzmZo4aA@kesavan.db.elephantsql.com/yeibjham",
         // `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
         { logging: false }
@@ -65,3 +49,21 @@ module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
   conn: sequelize,     // para importart la conexión { conn } = require('./db.js');
 };
+
+
+// process.env.NODE_ENV === "production"
+// ? new Sequelize({
+//     database: DB_NAME,
+//     dialect: "postgres",
+//     host: DB_HOST,
+//     port: 5432,
+//     username: DB_USER,
+//     password: DB_PASSWORD,
+//     pool: { max: 3, min: 1, idle: 10000 },
+//     dialectOptions: {
+//       ssl: { require: true, rejectUnauthorized: false },
+//       keepAlive: true,
+//     },
+//     ssl: true,
+//   })
+// : 
